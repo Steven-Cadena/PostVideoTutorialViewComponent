3377c1da3375f0857f82fe8f02052138e8918cfe
Steven-Cadena <steven.cadena.giler@gmail.com>
2022-03-03 09:59:27 +0100

    Add project files.

---
+++ PostVideoTutorialViewComponent/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PostVideoTutorialViewComponent.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
